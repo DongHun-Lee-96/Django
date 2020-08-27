97cec324faf3ef6cda6b5bc5e5fc379ff2048213
DongHun Lee <ldh960102@gmail.com>
2020-08-27 09:34:49 +0900

    polls buttons added

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),  # index is used for redirect later
     path('<int:question_id>/', views.detail, name='detail'),
-    path('vote/<int:question_id>/', views.vote, name='vote')
+    path('vote/<int:question_id>/', views.vote, name='vote'),
+    path('reset/<int:question_id>/', views.reset, name='reset')
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,6 +5,17 @@ from .models import Question, Choice
 # urls.py 내에서 question_id라는 명칭을 사용했기 때문에
 
 
+def reset(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    choices = question.choice_set.all()
+    for choice in choices:  # for-each
+        choice.votes = 0
+        choice.save()
+
+    return redirect('/polls/'+str(question_id))
+    # return redirect('/polls/%s' % question_id)
+
+
 def vote(request, question_id):
     # 사용자가 선택한 radio 값
     choice_id = request.GET.get('choice')
